4c5551338d2e0fc4a0c45d69a3e8a5109a8dfae7
jeromecabugwason <jeromecabugwason2@protonmail.com>
2023-04-15 01:08:02 +0800

    add: command entrypoint

--- lib/index.ts
+++ lib/index.ts
@@ -1,29 +1,57 @@
+import fs from 'fs/promises';
+import { stringify } from 'yaml';
+
+import { Project } from './core/project/project.js';
 import { ProjectRepositoryImpl } from './dataAccess/repositories/projectRepositoryImpl.js';
 import { PromptServiceImpl } from './serviceAgent/services/promptServiceImpl.js';
 import { GitServiceImpl } from './serviceAgent/services/gitServiceImpl.js';
+import { CreateProjectInteractor } from './core/project/create/createProjectInteractor.js';
 import { CloneProject } from './core/workflows/project/clone/cloneProject.js';
 import { CreateProject } from './core/workflows/project/create/createProject.js';
-
-import { stringify } from 'yaml';
-import { Project } from './core/project/project.js';
-import fs from 'fs/promises';
-import { CreateProjectInteractor } from './core/project/create/createProjectInteractor.js';
 import { errorLogger } from './core/util/errorLogger.js';
-async function main() {
-  const projectRepository = new ProjectRepositoryImpl('./data/projects.yml');
-  const promptService = new PromptServiceImpl();
-  const gitService = new GitServiceImpl();
-  const createProject = new CreateProjectInteractor(projectRepository);
 
-  const projectCloner = new CloneProject(
-    projectRepository,
-    gitService,
-    promptService
-  );
-  const projectCreator = new CreateProject(promptService, createProject);
+function processArgs() {
+  const args = process.argv.slice(2);
+
+  const command = args[0];
+
+  switch (command) {
+    case 'clone':
+      const outputPath = args[1];
+      return { command, outputPath };
+    case '--create-project':
+      return { command };
+    default:
+      throw new Error(`Invalid command: ${command}`);
+  }
+}
+
+async function main() {
   try {
-    // await projectCloner.cloneSelectedProjects('./cloned-projects');
-    await projectCreator.createProject();
+    const { command, outputPath } = processArgs();
+
+    const projectRepository = new ProjectRepositoryImpl('./data/projects.yml');
+    const promptService = new PromptServiceImpl();
+    const gitService = new GitServiceImpl();
+    const createProject = new CreateProjectInteractor(projectRepository);
+
+    const projectCloner = new CloneProject(
+      projectRepository,
+      gitService,
+      promptService
+    );
+    const projectCreator = new CreateProject(promptService, createProject);
+
+    switch (command) {
+      case 'clone':
+        await projectCloner.cloneSelectedProjects(outputPath);
+        break;
+      case '--create-project':
+        await projectCreator.createProject();
+        break;
+      default:
+        throw new Error(`Invalid command: ${command}`);
+    }
   } catch (error) {
     errorLogger(error);
   }
